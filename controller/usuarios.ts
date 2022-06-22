cfce067b0b7d02f736de61b3b5310d9e2864b124
Joao Ormeño <joao23.1996@gmail.com>
2022-06-22 17:45:28 -0400

    metodo post

--- controller/usuarios.ts
+++ controller/usuarios.ts
@@ -34,41 +34,84 @@ export const postUsuario = async ( req: Request, res: Response ) => {
     const { body } = req;
 
     try {
-        const usuario = new Usuario( body );
+        
+        const existeEmail = await Usuario.findOne({
+            where: {
+                email: body.email
+            }
+        });
+
+        if (existeEmail) {
+            return res.status(400).json({
+                msg: 'Ya existe un usuario con el email ' + body.email
+            });
+        }
+
+
+        const usuario = new Usuario(body);
         await usuario.save();
 
         res.json( usuario );
-    } catch ( error ) {
-        console.log( error );
-        res.status( 500 ).json({
-            msg:'Hable con el administrador'
+
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        })    
+    }
+
+
+}
+
+
+export const putUsuario = async (req: Request, res: Response) => {
+
+    const { id }   = req.params;
+    const { body } = req;
+
+    try {
+        
+        const usuario = await Usuario.findByPk( id );
+        if ( !usuario ) {
+            return res.status(404).json({
+                msg: 'No existe un usuario con el id ' + id
+            });
+        }
+
+        await usuario.update( body );
+
+        res.json( usuario );
+
+
+    } catch (error) {
+
+        console.log(error);
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        })    
+    }  
+
+}
+
+
+export const deleteUsuario = async (req: Request, res: Response) => {
+
+    const { id } = req.params;
+
+    const usuario = await Usuario.findByPk( id );
+    if ( !usuario ) {
+        return res.status(404).json({
+            msg: 'No existe un usuario con el id ' + id
         });
     }
 
-}
+    await usuario.update({ estado: false });
+
+    // await usuario.destroy();
 
 
-export const putUsuario = (req: Request, res: Response) => {
-
-    const {id} = req.params;
-    const {body} = req;
-
-    res.json({
-        msg: 'putUsuarios',
-        body,
-        id
-    });
-
-}
-
-
-export const deleteUsuario = (req: Request, res: Response) => {
-
-    const {id} = req.params;
-
-    res.json({
-        msg: 'deleteUsuarios',
-        id
-    });
+    res.json(usuario);
 
 }
