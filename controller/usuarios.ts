a0fc44a6bb4dc8b6ea65e629ba73a8a8290a1871
Joao Ormeño <joao23.1996@gmail.com>
2022-06-22 17:45:57 -0400

    metodo put

--- controller/usuarios.ts
+++ controller/usuarios.ts
@@ -96,22 +96,13 @@ export const putUsuario = async (req: Request, res: Response) => {
 }
 
 
-export const deleteUsuario = async (req: Request, res: Response) => {
+export const deleteUsuario = (req: Request, res: Response) => {
 
-    const { id } = req.params;
+    const {id} = req.params;
 
-    const usuario = await Usuario.findByPk( id );
-    if ( !usuario ) {
-        return res.status(404).json({
-            msg: 'No existe un usuario con el id ' + id
-        });
-    }
-
-    await usuario.update({ estado: false });
-
-    // await usuario.destroy();
-
-
-    res.json(usuario);
+    res.json({
+        msg: 'deleteUsuarios',
+        id
+    });
 
 }
